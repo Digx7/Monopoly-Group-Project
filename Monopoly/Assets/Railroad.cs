b251ba45382422ff21aee32899f6c4e210ddd863
DYLAB2331 <dylannguyen2331@gmail.com>
2023-11-14 23:29:53 -0800

    Added Ethan's Railroad and Utility class files

---
+++ Monopoly/Assets/Railroad.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+public class Railroad : Tile
+{
+    private bool hasCard1 = false; 
+    private bool hasCard2 = false; 
+    private bool hasCard3 = false; 
+    private bool hasCard4 = false;
+    //UNCOMMENT WHEN MERGED
+    //private int RailroadPrice = GetTileCost();
+
+
+
+    public void SetCard1(bool card)
+    {
+        hasCard1 = card;
+    }
+
+    public void SetCard2(bool card)
+    {
+        hasCard2 = card;
+    }
+
+    public void SetCard3(bool card)
+    {
+        hasCard3 = card;
+    }
+
+    public void SetCard4(bool card)
+    {
+        hasCard4 = card;
+    }
+
+    public int GetRent()
+    {
+        int multiplier = 0;
+        if(hasCard1)
+        {
+            multiplier++;
+        }
+        if(hasCard2)
+        {
+            multiplier++;
+        }
+        if (hasCard3)
+        {
+            multiplier++;
+        }
+        if (hasCard4)
+        {
+            multiplier++;
+        }
+        return 0;
+            //RailroadPrice * multiplier;
+            //UNCOMMENT THIS WHEN MERGED
+    }
+}
---
+++ Monopoly/Assets/Utility.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Utility : Tile
+{
+
+    private bool hasCard1 = false;
+    private bool hasCard2 = false;
+    //UNCOMMENT WHEN MERGED
+    //private int UtilityCost = GetTileCost();
+
+    public void SetCard1(bool card) 
+    {
+        hasCard1 = card;
+    }
+
+    public void SetCard2(bool card)
+    {
+        hasCard1 = card;
+    }
+
+    //UNCOMMENT THIS WHEN THIS IS MERGED
+    int CheckCardPossession()
+    {
+        /* DiceRoll dice = new DiceRoll();
+         if (hasCard1 && hasCard2) //if player has both cards
+         {
+             return dice * 10; //roll dice times 10
+         }
+         else if (hasCard1 || hasCard2) // checks to see if player has at least one card
+         {
+             return dice * 4; //roll dice times 4
+         }
+         */
+        return 0;
+    }
+    public int GetUtilityCost()
+    {
+        return GetTileCost();
+    }
+}
